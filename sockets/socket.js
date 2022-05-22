72b976504fbfaf901d97043c02103bda94cad77c
Miguel Angel Martinez Puga <muckystack@gmail.com>
2022-05-22 18:49:41 -0500

    Se agrega mensaje de actualizacion

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const { io } = require('../index');
+const Band = require('../models/Band.model');
+const Bands = require('../models/Bands.model');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('ACDC'));
+
+console.log(bands);
 
 // Mensajes de sockets
 io.on('connection', (client) => {
   console.log('Cliente conectado');
+
+  client.emit('active-bands', bands.getBands());
 
   client.on('disconnect', () => {
     console.log('Cliente desconectado');
